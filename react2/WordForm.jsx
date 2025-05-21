d94ecf02c3ca0799c007eb762f6b9fb838ebf507
dlgPfla <132207550+dlgPfla@users.noreply.github.com>
2025-05-21 14:48:34 +0900

    Update WordForm.jsx

--- react2/WordForm.jsx
+++ react2/WordForm.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-// 새로운 단어를 추가하는 입력 폼
+
 function WordForm(props) {
   const [word, setWord] = useState("");       // 단어 입력 상태
   const [meaning, setMeaning] = useState(""); // 뜻 입력 상태
 
-  // 폼 제출 시 실행되는 함수
+  // 근데 안됨 이상함!!!!
   const handleSubmit = (event) => {
     event.preventDefault(); // 페이지 새로고침 방지
 
@@ -15,10 +15,10 @@ function WordForm(props) {
       return;
     }
 
-    // 부모 컴포넌트(App)에서 전달받은 onAdd 함수 호출
+    // 
     props.addWord(word, meaning);
 
-    // 입력창 초기화
+    // 
     setWord("");
     setMeaning("");
   };
