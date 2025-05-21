5c7ff3ea04ba771372a2560cf74302bc52b14abe
dlgPfla <132207550+dlgPfla@users.noreply.github.com>
2025-05-21 14:47:30 +0900

    Update WordCard.jsx

--- react2/WordCard.jsx
+++ react2/WordCard.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import styles from "../style/WordCard.module.css";
-// 단어 카드 컴포넌트
+
 function WordCard({ word, onToggleMemorized }) {
   const { id, word: wordText, meaning, memorized } = word;
 
   
-    // memorized가 true이면 memorized 클래스 추가
+    
     const cardClassName = word.memorized
       ? `${styles.card} ${styles.memorized}`
       : styles.card;
