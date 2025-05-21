a57c8c37dba9dfce4745bdf3feb30b5b5325e711
maetel <handsson@ewhain.net>
2025-05-21 13:53:00 +0900

    feat: add react-router-dom

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import Hello from './component/Hello';
-import Welcome from './component/Welcome';
-import styles from './App.module.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './component/Header';
+import DayList from './component/DayList';
+import Day from './component/Day';
+import EmptyPage from './component/EmptyPage';
 
-function App() { // index.js이 사용중
+function App() {
   return (
-    <div className="App">
-      <Hello />
-      <Welcome />
-      <Hello />
-      <Hello />
-      <div className={styles.box}>App.module.css</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- voca/src/component/World.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-    return <h1>World is spinning</h1>;
-}
